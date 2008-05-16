9ad95636d9db8279b165b834177821c60c65f454
dan <dan@ubuntu.(none)>
2008-05-16 18:11:54 -0400

    Radiant version 0.6.7 and greater include the Shards extension. Checking for it causes this plugin not to load.

--- copy_move_extension.rb
+++ copy_move_extension.rb
@@ -11,7 +11,7 @@ class CopyMoveExtension < Radiant::Extension
   end
   
   def activate
-    raise "The Shards extension is required and must be loaded first!" unless defined?(Shards)
+#    raise "The Shards extension is required and must be loaded first!" unless defined?(Shards)
     admin.page.index.add :sitemap_head, 'copy_move_extra_th'
     admin.page.index.add :node, 'copy_move_extra_td', :after => "add_child_column"
   end
