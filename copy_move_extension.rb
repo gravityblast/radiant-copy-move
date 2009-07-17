6e33a95f799d0763e0580159aa513d43a21ba742
Sean Cribbs <seancribbs@gmail.com>
2009-07-18 03:28:06 +0800

    Using a before filter instead of adding stylesheet in view prevents spec failures in chronicle and is more DRY.
    Signed-off-by: Andrea Franz <andrea@gravityblast.com>

--- copy_move_extension.rb
+++ copy_move_extension.rb
@@ -4,19 +4,24 @@ class CopyMoveExtension < Radiant::Extension
   version "1.9.1"
   description "Adds the ability to copy and move a page and all of its children"
   url "http://gravityblast.com/projects/radiant-copymove-extension/"
-    
+
   define_routes do |map|
     map.copy_move_index       'admin/pages/copy_move/:id',            :controller => 'copy_move', :action => 'index'
     map.copy_move_copy_move   'admin/pages/copy_move/:id/copy_move',  :controller => 'copy_move', :action => 'copy_move'
   end
-  
+
   def activate
-#    raise "The Shards extension is required and must be loaded first!" unless defined?(Shards)
+    Admin::PagesController.class_eval do
+      before_filter do |c|
+        c.include_stylesheet 'admin/copy_move'
+      end
+    end
+
     admin.page.index.add :sitemap_head, 'copy_move_extra_th'
     admin.page.index.add :node, 'copy_move_extra_td', :after => "add_child_column"
   end
-  
+
   def deactivate
   end
-  
+
 end
